13427f38a4083393199ed07abaf690229da1f87b
pablo <pablovicenteroura2005@gmail.com>
2025-04-22 20:28:55 +0200

    commit

--- server/public/web/flutter_service_worker.js
+++ server/public/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "83e774db24a6323be5f5744ea484d878
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ff5a1ad6b938c523bd4bf651e06fad2d",
+"flutter_bootstrap.js": "e944e72e588b6df8b6d720582241446c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
