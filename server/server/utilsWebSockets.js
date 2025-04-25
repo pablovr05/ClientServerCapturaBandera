374b1394258422360e2d2ced5a55fb804865a808
pablo <pablovicenteroura2005@gmail.com>
2025-04-25 16:10:04 +0200

    commit

--- server/server/utilsWebSockets.js
+++ server/server/utilsWebSockets.js
@@ -28,7 +28,7 @@ class Obj {
     // A websocket client connects
     newConnection(con) {
 
-        const clientIP = con._socket.remoteAddress;
+        const clientIP = con.headers['x-forwarded-for'] || con._socket.remoteAddress;
 
         // Si la dirección tiene el formato ::ffff:<IP>, extraemos solo la dirección IPv4
         const ipv4Regex = /^::ffff:(\d+\.\d+\.\d+\.\d+)$/;
@@ -40,6 +40,7 @@ class Obj {
         }
 
         console.log("Client connected with IP: " + clientIpAddress);
+
     
         // Generar ID únic per al client
         const id = "C" + uuidv4().substring(0, 5).toUpperCase();
