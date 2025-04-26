725fc09c48e8e94e000fe13b78b8739e07560d9d
pablo <pablovicenteroura2005@gmail.com>
2025-04-26 13:06:09 +0200

    interfaz de login y register

--- client/core/src/main/java/com/project/screens/LoginScreen.java
+++ client/core/src/main/java/com/project/screens/LoginScreen.java
@@ -6,8 +6,10 @@ import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.*;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class LoginScreen implements Screen {
 
@@ -15,7 +17,6 @@ public class LoginScreen implements Screen {
     private Stage stage;
     private BitmapFont font;
     private SpriteBatch batch;
-
     private Texture backgroundTexture;
 
     private Table table;
@@ -38,10 +39,17 @@ public class LoginScreen implements Screen {
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        font = new BitmapFont();
-        font.getData().setScale(1.2f);
+        // Cargar fuente TTF bonita
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Roboto-Italic-VariableFont_wdth,wght.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 32; // tamaño grande y nítido
+        parameter.color = Color.WHITE;
+        parameter.minFilter = Texture.TextureFilter.Linear;
+        parameter.magFilter = Texture.TextureFilter.Linear;
+        font = generator.generateFont(parameter);
+        generator.dispose();
 
-        backgroundTexture = new Texture("fondo.png"); // <-- fondo
+        backgroundTexture = new Texture("fondo3.gif");
 
         createUI();
     }
@@ -60,18 +68,29 @@ public class LoginScreen implements Screen {
         Texture whiteTexture = new Texture(pixmap);
         pixmap.dispose();
 
+        // Estilo para el título
         Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE);
 
+        // Estilo para el fondo de los botones
+        Texture buttonTexture = new Texture("button.png"); // Añade tu imagen de fondo
+        Drawable buttonBackground = new TextureRegionDrawable(new TextureRegion(buttonTexture));
+
+        // Estilo para los TextFields
         TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
         textFieldStyle.font = font;
         textFieldStyle.fontColor = Color.BLACK;
         textFieldStyle.background = new TextureRegionDrawable(new TextureRegion(whiteTexture));
 
+        // Estilo para los botones
         TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
         buttonStyle.font = font;
+        buttonStyle.up = buttonBackground; // Asignar fondo a los botones
+        buttonStyle.down = buttonBackground; // Fondo cuando el botón es presionado
 
+        // Crear el título
         titleLabel = new Label("Login / Registro", labelStyle);
 
+        // Crear los campos de texto
         nicknameField = new TextField("", textFieldStyle);
         nicknameField.setMessageText("Nickname");
 
@@ -86,11 +105,13 @@ public class LoginScreen implements Screen {
         passwordField.setPasswordCharacter('*');
         passwordField.setPasswordMode(true);
 
+        // Crear los botones
         confirmButton = new TextButton("Confirmar", buttonStyle);
         loginModeButton = new TextButton("Modo Login", buttonStyle);
         registerModeButton = new TextButton("Modo Registro", buttonStyle);
         guestButton = new TextButton("Entrar como Invitado", buttonStyle);
 
+        // Listener para los botones
         confirmButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -148,14 +169,17 @@ public class LoginScreen implements Screen {
         }
 
         table.add(passwordField).colspan(2).height(50).padBottom(25).expandX().fillX().row();
-        table.add(confirmButton).colspan(2).height(50).padBottom(25).expandX().fillX().row();
-        table.add(loginModeButton).height(50).pad(5).expandX().fillX();
-        table.add(registerModeButton).height(50).pad(5).expandX().fillX().row();
-        table.add(guestButton).colspan(2).height(50).padTop(20).expandX().fillX();
+
+        // Botones con mayor altura
+        table.add(confirmButton).colspan(2).height(80).padBottom(25).expandX().fillX().row();  // Aumenté la altura a 70
+        table.add(loginModeButton).height(80).pad(5).expandX().fillX();  // Aumenté la altura a 70
+        table.add(registerModeButton).height(80).pad(5).expandX().fillX().row();  // Aumenté la altura a 70
+        table.add(guestButton).colspan(2).height(80).padTop(20).expandX().fillX();  // Aumenté la altura a 70
 
         stage.clear();
         stage.addActor(container);
     }
+
 
     @Override
     public void show() {}
@@ -164,7 +188,6 @@ public class LoginScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
 
-        // Dibujar fondo antes de la UI
         batch.begin();
         batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
@@ -195,6 +218,6 @@ public class LoginScreen implements Screen {
         stage.dispose();
         batch.dispose();
         font.dispose();
-        backgroundTexture.dispose(); // <-- no olvides liberar
+        backgroundTexture.dispose();
     }
 }
